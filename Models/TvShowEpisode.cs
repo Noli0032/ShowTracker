c42cebd425ebdb24bd6891e500aaa135bcab6da7
Noa <noa.lindberg@gmail.com>
2026-05-19 11:30:54 +0200

    created model for a TvShowEpisode

---
+++ Models/TvShowEpisode.cs
@@ -0,0 +1,14 @@
+namespace MyProject.Models;
+
+public record TvShowEpisode
+{
+    public int Id {get; init;}
+    public required string Name {get; init;}
+    public required int Season {get; init;}
+    public required int Number {get; init;}
+    public DateOnly? Airdate {get; init;}
+    public int? Runtime {get; init;}
+    public TvShowRating? Rating {get; init;}
+    public TvShowImage? Image {get; init;}
+    public required string Summary {get; init;}
+}
