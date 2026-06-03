97bd163067162b0a9611a210e5e98e7515916e6c
Noa <noa.lindberg@gmail.com>
2026-06-03 13:21:25 +0200

    Add tests folder and first unit test for TvMazeService

---
+++ tests/ShowTracker.tests/TvMazeServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.Extensions.Logging.Abstractions;
+using ShowTracker.Models;
+using ShowTracker.Services;
+using ShowTracker.Tests.Helpers;
+
+namespace ShowTracker.tests;
+
+[TestClass]
+public sealed class TvMazeServiceTests
+{
+    private static TvMazeService CreateService(string json)
+    {
+        var response = new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent(json, System.Text.Encoding.UTF8, "application/json")
+        };
+
+        var httpClient = new HttpClient(new FakeHttpMessageHandler(response))
+        {
+            BaseAddress = new Uri("https://api.tvmaze.com")
+        };
+
+        var logger = NullLogger<TvMazeService>.Instance;
+        return new TvMazeService(httpClient, logger);
+    }
+
+    [TestMethod]
+    public async Task GetTvShowsByPageAsync_WhenApiRespondsWithData_ReturnsShows()
+    {
+        // Arrange
+        var json = JsonSerializer.Serialize(new []
+        {
+            new TvShow {Id = 1, Name = "Breaking Bad", Language = "English", Status = "Ended"},
+            new TvShow {Id = 2, Name = "Better Call Saul", Language = "English", Status = "Ended"}
+        });
+        var service = CreateService(json);
+
+        // Act
+        var result = await service.GetTvShowsByPageAsync(1);
+
+        // Assert
+        Assert.HasCount(2, result);
+    }
+}
